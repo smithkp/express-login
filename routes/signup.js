8cc4d800a1c3cda2e2adb3c11ce60231f7bf2c03
Kyle <kyles6688@gmail.com>
2020-02-26 17:23:25 -0500

    changes made to store users in a local variable

--- routes/signup.js
+++ routes/signup.js
@@ -1,12 +1,29 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcrypt');
+
+const users =[];
+
+
 
 router.get('/', (req, res, next) =>{
     res.render('signup');
 });
 
-router.post('/signup', (req, res) =>{
-
+router.post('/', async (req, res) =>{
+    try {
+        const hashedPass = await bcrypt.hash(req.body.password, 10);
+        users.push({
+            id: Date.now().toString(),
+            username: req.body.username,
+            email: req.body.email,
+            password: hashedPass
+        });
+        res.redirect('/login');
+    } catch (e) {
+        res.redirect('/signup');
+    }
+        console.log(users);
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var indexRouter = require('./routes/index');
+const bcrypt = require('bcrypt');
 
 var app = express();
 
